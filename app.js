3c5b359cf41cf7690a0fe94df902717889cecf31
jiasiqi <jiasiqi_web@163.com>
2020-04-29 17:39:01 +0800

    master

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const express = require('express'),
       //mongoose内依赖自动有mongodb,所以不需要再次安装或引入mongodb
       mongoose = require('mongoose'),
-      cookieSession = require('cookie-session');
+      cookieSession = require('cookie-session'),
+      loginCtrl = require('./controllers/loginCtrl');
 
 //链接数据库 端口号不需要写,最后的反斜杠是操作的数据库名称
 mongoose.connect('mongodb://localhost/sm',{useUnifiedTopology:true,useNewUrlParser:true});
@@ -18,6 +19,9 @@ app.use(cookieSession({
     maxAge : 30 * 60 * 1000 //30min
 }))
 //路径清单:
+app.get('/login',loginCtrl.showLogin); //访问登录页面
+app.post('/login',loginCtrl.doLogin); //访问登录接口 处理登录操作
+app.propfind('/login',loginCtrl.checkUser); //访问接口 验证用户名是否存在
 
 
 //处理静态资源请求:
