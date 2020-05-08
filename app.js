81b03483cac89fda98c794dd985431d8628c13db
jiasiqi <jiasiqi_web@163.com>
2020-05-08 16:10:12 +0800

    班级管理界面学生管理完成

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require('express'),
       //mongoose内依赖自动有mongodb,所以不需要再次安装或引入mongodb
       mongoose = require('mongoose'),
       cookieSession = require('cookie-session'),
-      loginCtrl = require('./controllers/loginCtrl');
-
+      router = require('./route');
 //链接数据库 端口号不需要写,最后的反斜杠是操作的数据库名称
 mongoose.connect('mongodb://localhost/sm',{useUnifiedTopology:true,useNewUrlParser:true});
 
@@ -16,16 +15,15 @@ app.set('view engine','ejs');
 app.use(cookieSession({
     name:'sess_id',
     keys:['key1'],
-    maxAge : 30 * 60 * 1000 //30min
+    maxAge : 60 * 60 * 1000 //30min
 }))
-//路径清单:
-app.get('/login',loginCtrl.showLogin); //访问登录页面
-app.post('/login',loginCtrl.doLogin); //访问登录接口 处理登录操作
-app.propfind('/login',loginCtrl.checkUser); //访问接口 验证用户名是否存在
-
 
 //处理静态资源请求:
 app.use(express.static('public'));
+
+
+//处理路由清单:
+app.use('/',router);
 
 //监听端口
 app.listen(3000);
